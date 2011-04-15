a65dca5f7343ef666cf0895798719e3fed48f88c
hiqdevlord <hiqdevlord@gmail.com>
2011-04-15 08:44:03 -0400

    removed scale and bias terms from the noise contrastive estimator

--- utils/utlc.py
+++ utils/utlc.py
@@ -21,22 +21,15 @@ from auc import embed
 
 floatX = theano.config.floatX
 
-def get_constant(variable, return_scalar=False):
-    """ Little hack to return the python value of a theano shared variable.
-
-    If return_scalar is True, and the constant is an ndarray of 0 dimensions,
-    the content of that ndarray will be returned instead.
-    """
+def get_constant(variable):
+    """ Little hack to return the python value of a theano shared variable """
     try:
-        ret = theano.function([],
-                              variable,
-                              mode=theano.compile.Mode(linker='py')
-                              )()
+        return theano.function([],
+                               variable,
+                               mode=theano.compile.Mode(linker='py')
+                               )()
     except TypeError:
-        ret = variable
-    if isinstance(ret, numpy.ndarray) and ret.ndim == 0:
-        ret = ret.item()
-    return ret
+        return variable
 
 def sharedX(value, name=None, borrow=False):
     """Transform value into a shared variable of type floatX"""
