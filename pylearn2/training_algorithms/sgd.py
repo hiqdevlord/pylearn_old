736df35602ec95442d21755c81dd97327d472248
hiqdevlord <hiqdevlord@gmail.com>
2012-02-07 21:45:04 -0500

    start of convolution support

--- pylearn2/training_algorithms/sgd.py
+++ pylearn2/training_algorithms/sgd.py
@@ -1,6 +1,5 @@
-from __future__ import division
 import numpy as np
-from theano import function, config
+from theano import function
 import theano.tensor as T
 from warnings import warn
 from pylearn2.monitor import Monitor
@@ -96,7 +95,13 @@ class SGD(TrainingAlgorithm):
                                  batches=self.monitoring_batches,
                                  batch_size=self.batch_size)
 
-        X = T.matrix(name='sgd_X')
+        space = self.model.get_input_space()
+
+        X = space.make_theano_batch(name='sgd_X')
+
+        self.topo = len(X.type.broadcastable) > 2
+
+
         J = self.cost(model, X)
         if J.name is None:
             J.name = 'sgd_cost(' + X.name + ')'
@@ -161,7 +166,10 @@ class SGD(TrainingAlgorithm):
             self.monitor()
         self.first = False
         for i in xrange(self.batches_per_iter):
-            X = dataset.get_batch_design(batch_size)
+            if self.topo:
+                X = dataset.get_batch_topo(batch_size)
+            else:
+                X = dataset.get_batch_design(batch_size)
 
             #print '\n----------------'
             self.sgd_update(X, self.learning_rate)
@@ -198,19 +206,15 @@ class UnsupervisedExhaustiveSGD(TrainingAlgorithm):
         self.monitoring_batches = monitoring_batches
         self.termination_criterion = termination_criterion
         self._register_update_callbacks(update_callbacks)
-        self.first = True
+        self.first = False
 
     def setup(self, model, dataset):
         self.model = model
         self.monitor = Monitor.get_monitor(model)
-        # TODO: monitoring batch size ought to be configurable
-        # separately from training batch size, e.g. if you would rather
-        # monitor on one somewhat big batch but update on many small
-        # batches.
         self.monitor.set_dataset(dataset=self.monitoring_dataset,
                                  batches=self.monitoring_batches,
                                  batch_size=self.batch_size)
-        X = T.matrix(name="%s[X]" % self.__class__.__name__)
+        X = T.matrix(name="%s(X)" % self.__class__.__name__)
         cost_value = self.cost(model, X)
         if cost_value.name is None:
             cost_value.name = 'sgd_cost(' + X.name + ')'
@@ -270,7 +274,7 @@ class UnsupervisedExhaustiveSGD(TrainingAlgorithm):
         design_matrix = dataset.get_design_matrix()
         # TODO: add support for reshuffling examples.
         for batch_slice in self.slice_iterator:
-            batch = np.cast[config.floatX](design_matrix[batch_slice])
+            batch = design_matrix[batch_slice]
             self.sgd_update(batch, self.learning_rate)
             self.monitor.batches_seen += 1
             self.monitor.examples_seen += batch_size
@@ -376,22 +380,6 @@ class EpochCounter(object):
     def __call__(self, model):
         self._epochs_done += 1
         return self._epochs_done < self._max_epochs
-
-
-class AnnealedLearningRate(object):
-    def __init__(self, anneal_start):
-        self._initialized = False
-        self._count = 0
-        self._anneal_start = anneal_start
-
-    def __call__(self, algorithm):
-        if not self._initialized:
-            self._base = algorithm.learning_rate
-        self._count += 1
-        algorithm.learning_rate = self.current_learning_rate()
-
-    def current_learning_rate(self):
-        return self._base * min(1, self._anneal_start / self._count)
 
 
 # This might be worth rolling into the SGD logic directly at some point.
