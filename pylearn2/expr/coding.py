573971633d37afab575ee1a0056698193bc05b0b
hiqdevlord <hiqdevlord@gmail.com>
2012-03-05 15:40:28 -0500

    Added the new split datasets features to the dense design matrix.

--- pylearn2/expr/coding.py
+++ pylearn2/expr/coding.py
@@ -1,7 +1,6 @@
 """ Expressions for encoding features """
 
 import theano.tensor as T
-from theano.printing import Print
 
 def triangle_code(X, centroids):
     """ Compute the triangle activation function used
@@ -19,15 +18,7 @@ def triangle_code(X, centroids):
     Xc = T.dot(X, centroids.T)
     Xc.name = 'Xc'
 
-    sq_dists =  c_sqr + X_sqr - 2. * Xc
-
-    #TODO: why do I have to do this and Adam doesn't?
-    #is it just because he uses float64 and I usually use
-    #float32? or are our libraries numerically unstable somehow,
-    #or does matlab handle sqrt differently?
-    sq_dists_safe = T.clip(sq_dists,0.,1e30)
-
-    Z = T.sqrt( sq_dists_safe)
+    Z = T.sqrt( c_sqr + X_sqr - 2. * Xc)
     Z.name = 'Z'
 
     mu = Z.mean(axis=1)
@@ -40,5 +31,3 @@ def triangle_code(X, centroids):
     rval.name = 'triangle_code'
 
     return rval
-
-
